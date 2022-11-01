2ef8a7ca0f529daff97b1e296b920e3829ccb3b4
Julio-Alejandro-Mazo <juliomazo10@javerianacali.edu.co>
2022-10-31 23:16:36 -0500

    MOD: se modificaron partes de a columna

--- pages/PruebaPage.py
+++ pages/PruebaPage.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 def probar_streamlit():
 
-    col1, col2 = st.columns(3)
+    col1, col2 = st.columns(2)
 
     with col1:
         st.header("Hello")
@@ -18,7 +18,7 @@ def probar_streamlit():
         genre = st.radio(
             "Cuál es tu película favorita",
             ('Jhon Wick', 'Bob Esponja', 'Black Adam'))
-        
+
         if genre == 'Jhon wick':
             st.write('Selecionaste Jhon Wick.')
         else:
