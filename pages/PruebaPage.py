8c1dae8b1bc41b6c7e6f720d0208df1cbd9f5723
Julio-Alejandro-Mazo <juliomazo10@javerianacali.edu.co>
2022-10-31 22:59:36 -0500

    NEW: se agregaron nuevas funcionalidades

--- pages/PruebaPage.py
+++ pages/PruebaPage.py
@@ -1,14 +1,25 @@
 import streamlit as st
 
 def probar_streamlit():
+
+    st.write("Bienvenidos")
+    if st.button('¿Como estan?'):
+        st.write('¿Qué estan haciendo?')
+    else:
+        st.write('Goodbye\n')
+
     """ Ponga aqui todos los controles de prueba para que entienda como funciona"""
     st.write("Agregue aquí botones, paneles, y opciones tal como se describe en el readme")
-    st.button("Soy un boton")
+    st.button("Soy un boton\n")
 
-    if st.button('Say hello'):
-        st.write('Why hello there')
+    genre = st.radio(
+        "Cuál es tu película favorita",
+        ('Jhon Wick', 'Bob Esponja', 'Black Adam'))
+
+    if genre == 'Jhon wick':
+        st.write('Selecionaste Jhon Wick.')
     else:
-        st.write('Goodbye')
+        st.write("Eres la Bestia")
 
 
 # Main call
